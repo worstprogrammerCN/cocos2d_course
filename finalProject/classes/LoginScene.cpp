d78b54dcb16c224268c96512f2975d813c7af2fa
worstprogrammerCN <657278923@qq.com>
2018-01-22 00:32:16 +0800

    add final project(涂鸦跳跃低配版)

---
+++ finalProject/classes/LoginScene.cpp
@@ -0,0 +1,69 @@
+#include "LoginScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "json/rapidjson.h"
+#include "json/document.h"
+#include "json/writer.h"
+#include "json/stringbuffer.h"
+#include "Global.h"
+#include "GameScene.h"
+#include <regex>
+using std::to_string;
+using std::regex;
+using std::match_results;
+using std::regex_match;
+using std::cmatch;
+using namespace rapidjson;
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+
+#include "json/document.h"
+#include "json/writer.h"
+#include "json/stringbuffer.h"
+using namespace  rapidjson;
+
+Scene* LoginScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = LoginScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+
+
+
+
+// on "init" you need to initialize your instance
+bool LoginScene::init()
+{
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+
+    Size size = Director::getInstance()->getVisibleSize();
+    visibleHeight = size.height;
+    visibleWidth = size.width;
+
+    textField = TextField::create("Player Name", "Arial", 30);
+    textField->setPosition(Size(visibleWidth / 2, visibleHeight / 4 * 3));
+    this->addChild(textField, 2);
+
+    auto button = Button::create();
+    button->setTitleText("Login");
+    button->setTitleFontSize(30);
+    button->setPosition(Size(visibleWidth / 2, visibleHeight / 2));
+    this->addChild(button, 2);
+
+    return true;
+}
+
